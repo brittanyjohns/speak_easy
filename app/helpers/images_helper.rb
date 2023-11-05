be8facdb3e3bb11afc7c89269539cbabb3a24ba0
Brittany Johns <britt@Brittanys-MacBook-Air.local>
2023-11-04 20:28:33 -0400

    fixing the things & stuff 2

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -21,14 +21,8 @@ module ImagesHelper
     image_tag(image.display_image.representation(resize_to_limit: [100, 100]).processed.url, data: { action: "click->speech#speak" })
   end
 
-<<<<<<< Updated upstream
-  def select_image_button(board, image, size: "thumnail", img_class: "rounded-full")
-    puts "img_class: #{img_class}"
-    unless image.cropped_image.attached?
-=======
   def select_image_button(board, image, size: "thumnail")
     unless image.cropped_image.attached? || image.saved_image.attached?
->>>>>>> Stashed changes
       puts "no image"
       return ""
     end
